14e18fc36b0129f81b2eed29ae8a6e0c5eba8cb7
mariokartmaster69 <72568227179@szily.hu>
2025-03-11 08:05:28 +0100

    1.tétel elkészült

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
+<?php
+$tombBe=array("B","B");
+$tombKi=array("","","","");
+$Betu="A";
+$indexMax=count(value: $tombBe)-1;
+for($i=0;$i<=$indexMax;$i++)
+{
+    $tombKi[$i]=$tombBe[$i].$Betu;
+    $tombKi[$i]=$tombKi[$i].$tombKi[$i];
+}
 
+print_r(value:$tombBe); print"<br>";
+print_r(value:$tombKi); print"<br>";
